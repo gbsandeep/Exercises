8e797076ae323ac7bfe1e68033f545b8c132b195
gbsandeep <gbsandeep@gmail.com>
2014-04-24 16:29:31 -0700

    Balanced Tree verification

--- Learning/Tree/BinaryTree.cs
+++ Learning/Tree/BinaryTree.cs
@@ -5,24 +5,52 @@ namespace UnitTestProject1 {
     [TestClass]
     public class UTBinaryTree {
         [TestMethod]
-        public void TestMethod1() {
+        public void BalancedTree() {
+            var root = new Node('a', new Node('b', new Node('d', null, null), new Node('e', null, null)), new Node('c', null, null));
+            var bt = new BinaryTree(root);
+            Assert.AreEqual(true, bt.IsBalanced());
+        }
+
+        [TestMethod]
+        public void NonBalancedTree() {
+            var root = new Node('a', new Node('b', new Node('d', null, null), new Node('e', new Node('f', null, null), null)), new Node('c', null, null));
+            var bt = new BinaryTree(root);
+            Assert.AreEqual(false, bt.IsBalanced());
         }
     }
 
-    class BinaryTreeNode : IComparable<BinaryTreeNode> {
-        public BinaryTreeNode() {
-
+    class Node {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public char Value { get; set; }
+        public Node() : this(' ', null, null) {
         }
 
-        public int CompareTo(BinaryTreeNode other) {
-            throw new NotImplementedException();
+        public Node(char value, Node left, Node right) {
+            this.Value = value;
+            this.Left = left;
+            this.Right = right;
         }
     }
 
-    class BinaryTree : IComparable<BinaryTree> {
-
-        public int CompareTo(BinaryTree other) {
-            throw new NotImplementedException();
+    class BinaryTree {
+        Node root;
+        public BinaryTree(Node root) {
+            this.root = root;
         }
+        bool isBalanced { get; set; }
+        public bool IsBalanced() {
+            isBalanced = true;
+            Depth(root);
+            return isBalanced;
+        }
+
+        private int Depth(Node node) {
+            var leftDepth = node.Left == null ? 0 : Depth(node.Left);
+            var rightDepth = node.Right == null ? 0 : Depth(node.Right);
+            if (Math.Abs(leftDepth - rightDepth) > 1) { isBalanced = false; }
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+
     }
 }
